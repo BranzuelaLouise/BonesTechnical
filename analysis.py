fb02bea8b711f799a6a0e5c852b261977cdedef9
Louise Branzuela <louisebranz@gmail.com>
2025-05-22 19:38:41 +1200

    Add compute_filler_ratio(text)

--- analysis.py
+++ analysis.py
@@ -1,12 +1,22 @@
 import pandas as pd
+import spacy
 from transformers import pipeline
+
+FILLER_WORDS = [
+     "um", "uh", "like", "you know", "i mean", "well", "so", "basically",
+     "actually", "er", "ah", "hmm", "hm", "you know what i mean", "you know",
+     "for sure"
+]
+
+model = "distilbert/distilbert-base-uncased-finetuned-sst-2-english" # Prevent clogging the standard output
 
 def main():
     lines = load_transcript("transcript.txt")
 
     for line in lines:
         sentiment = compute_sentiment(line)
-        print(sentiment)
+        filler_ratio = compute_filler_ratio(line)
+        print(f"Sentiment: {sentiment['label']}, Filler Ratio: {filler_ratio}")
 
 def load_transcript(file_path):
     with open(file_path, "r") as file:
@@ -16,11 +26,26 @@ def load_transcript(file_path):
     return lines
 
 def compute_sentiment(text):
-        sentiment_pipeline = pipeline("sentiment-analysis")
+        sentiment_pipeline = pipeline("sentiment-analysis", model=model)
         if not text or text.isspace():
             return {"label": "NEUTRAL", "score": 0.0}
         result = sentiment_pipeline(text)[0]
         return result
+
+def compute_filler_ratio(text):
+    total_words = len(text.split())
+
+    filler_count = 0
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text.lower())
+
+    for token in doc:
+         if token.text in FILLER_WORDS:
+              filler_count += 1
+              print(token.text)
+    
+    ratio = filler_count / total_words
+    return ratio
 
 if __name__ == "__main__":
     main()
